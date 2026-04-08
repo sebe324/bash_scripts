9e39e2a81bdd2d39e8ab464efa1e4c56e2ab0500
Sebastian Skonieczny <85199759@bat.com>
2026-04-08 11:47:50 +0200

    Changed dir path

--- 06_textprocessing/grep_basics.sh
+++ 06_textprocessing/grep_basics.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 
 # Search for lines containing Kubernetes
-grep "Kubernetes" "../mockdata/20220301-log.txt" | wc -l
+grep "Kubernetes" "../mock_data/20220301-log.txt" | wc -l
 
 # Ignore case
 
-grep -i "kubernetes" "../mockdata/20220301-log.txt" | wc -l
+grep -i "kubernetes" "../mock_data/20220301-log.txt" | wc -l
 
 # Find lines that do not have kubernetes
 
-grep -v "Kubernetes" "../mockdata/20220301-log.txt" | wc -l
+grep -v "Kubernetes" "../mock_data/20220301-log.txt" | wc -l
 
 # Search trough all files
 
-grep -r "Kubernetes" "../mockdata" | wc -l
+grep -r "Kubernetes" "../mock_data" | wc -l
 
 # Find logs from hour 22 to 23
-grep -E '^2[2-3]' "../mockdata/20220301-log.txt"
+grep -E '^2[2-3]' "../mock_data/20220301-log.txt"
 
 echo ""
 
 # Find logs from the 59th minute
-grep -E '^([01][0-9]|2[0-3]):59:[0-9]{2}' "../mockdata/20220301-log.txt"
+grep -E '^([01][0-9]|2[0-3]):59:[0-9]{2}' "../mock_data/20220301-log.txt"
 
 # Find logs from the morning that contain the keyword HTTP
 
-grep -E '^(0[0-9]|1[0-1]).*HTTP' "../mockdata/20220301-log.txt" | wc -l
+grep -E '^(0[0-9]|1[0-1]).*HTTP' "../mock_data/20220301-log.txt" | wc -l
 
 # Find files from the 10th day of the month
 
-ls -l "../mockdata" | grep -E "202[2-5]0(2|3)10"
+ls -l "../mock_data" | grep -E "202[2-5]0(2|3)10"
 
 # Find files between the 10th day and the 20th day of the month
 
-ls -l "../mockdata" | grep -E "202[2-5]0(2|3)(1[0-9]|20)"
+ls -l "../mock_data" | grep -E "202[2-5]0(2|3)(1[0-9]|20)"
 
--- 06_textprocessing/sed_basics.sh
+++ 06_textprocessing/sed_basics.sh
@@ -2,9 +2,9 @@
 
 # Replace PlayStation 4 with PS4
 
-sed 's/PlayStation 4/PS4/' ../mockdata/shop_data.csv
+sed 's/PlayStation 4/PS4/' ../mock_data/shop_data.csv
 
 # Replace PC with Personal Computer
 
-sed 's/PC/Personal Computer/' ../mockdata/shop_data.csv
+sed 's/PC/Personal Computer/' ../mock_data/shop_data.csv
 
