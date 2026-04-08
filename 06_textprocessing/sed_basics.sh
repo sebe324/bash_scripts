292d383de392ae8ae54b1ffed07728b876a80602
sebe324 <sebeq05@gmail.com>
2026-04-08 11:43:52 +0200

    Sed basics

---
+++ 06_textprocessing/sed_basics.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Replace PlayStation 4 with PS4
+
+sed 's/PlayStation 4/PS4/' ../mockdata/shop_data.csv
+
+# Replace PC with Personal Computer
+
+sed 's/PC/Personal Computer/' ../mockdata/shop_data.csv
+
